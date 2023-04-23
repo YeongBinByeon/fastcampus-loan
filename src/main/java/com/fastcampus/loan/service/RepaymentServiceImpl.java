c5b4601473f293990a179ee993473219b1b098d9
yeongbin <yeongbin6483@gmail.com>
2023-04-23 15:22:11 +0900

    ch 06. 대출 상환 기능 개발 / 05. 대출 상환 수정 기능 구현 커밋

--- src/main/java/com/fastcampus/loan/service/RepaymentServiceImpl.java
+++ src/main/java/com/fastcampus/loan/service/RepaymentServiceImpl.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -66,6 +67,50 @@ public class RepaymentServiceImpl implements RepaymentService {
         List<Repayment> repayments = repaymentRepository.findAllByApplicationId(applicationId);
 
         return repayments.stream().map( r-> modelMapper.map(r, RepaymentDTO.ListResponse.class)).collect(Collectors.toList());
+    }
+
+    @Override
+    public RepaymentDTO.UpdateResponse update(Long repaymentId, RepaymentDTO.Request request) {
+        Repayment repayment = repaymentRepository.findById(repaymentId).orElseThrow(()->{
+           throw new BaseException(ResultType.SYSTEM_ERROR);
+        });
+
+        // 전체 대출 잔고, 상환금액, 결과 대출 잔고
+        // 500 - 100 = 400
+        // 400 + 100 = 500
+        // 500 - 200 = 300
+
+        // 500 - 100 = 400
+        // 이전에 잘못 입력한 상환 금액(100) 가져오기
+        Long applicationId = repayment.getApplicationId();
+        BigDecimal beforeRepaymentAmount = repayment.getRepaymentAmount();
+
+        // 전체 대출 잔액에서 잘못 입력한 상환 금액 되돌리기 (+100)
+        balanceService.repaymentUpdate(applicationId,
+                BalanceDTO.RepaymentRequest.builder()
+                        .repaymentAmount(beforeRepaymentAmount)
+                        .type(BalanceDTO.RepaymentRequest.RepaymentType.ADD)
+                        .build()
+        );
+
+        repayment.setRepaymentAmount(request.getRepaymentAmount());
+        repaymentRepository.save(repayment);
+
+        // 전체 대줄 잔액에서 상환 요청한 금액만큼 상환하기 (-200)
+        BalanceDTO.Response updatedBalance = balanceService.repaymentUpdate(applicationId,
+                BalanceDTO.RepaymentRequest.builder()
+                        .repaymentAmount(request.getRepaymentAmount())
+                        .type(BalanceDTO.RepaymentRequest.RepaymentType.REMOVE)
+                        .build());
+
+        return RepaymentDTO.UpdateResponse.builder()
+                .applicationId(applicationId)
+                .beforeRepaymentAmount(beforeRepaymentAmount)
+                .afterRepaymentAmount(request.getRepaymentAmount())
+                .balance(updatedBalance.getBalance())
+                .createdAt(repayment.getCreatedAt())
+                .updatedAt(repayment.getUpdatedAt())
+                .build();
     }
 
     private boolean isRepayableApplication(Long applicationId){
