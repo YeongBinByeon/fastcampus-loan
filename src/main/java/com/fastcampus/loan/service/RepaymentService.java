b22f4acfc16f716b4a34c3240774f1f9ba17afa3
yeongbin <yeongbin6483@gmail.com>
2023-04-23 14:03:07 +0900

    ch 06. 대출 상환 기능 개발 / 04. 대출 상환 조회 기능 구현

--- src/main/java/com/fastcampus/loan/service/RepaymentService.java
+++ src/main/java/com/fastcampus/loan/service/RepaymentService.java
@@ -2,7 +2,11 @@ package com.fastcampus.loan.service;
 
 import com.fastcampus.loan.dto.RepaymentDTO;
 
+import java.util.List;
+
 public interface RepaymentService {
 
     RepaymentDTO.Response create(Long applicationId, RepaymentDTO.Request request);
+
+    List<RepaymentDTO.ListResponse> get(Long applicationId);
 }
